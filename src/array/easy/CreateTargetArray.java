22b146f68e2faedee6d958d6a9826cf82e186a36
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-03-08 22:13:25 +0400

    solved Create Target Array in the Given Order problem

---
+++ src/array/easy/CreateTargetArray.java
@@ -0,0 +1,31 @@
+package array.easy;
+
+import java.util.Arrays;
+
+// 1389
+public class CreateTargetArray {
+    public static void main(String[] args) {
+        int[] targetArray = createTargetArray(new int[]{1, 2, 3, 4, 0}, new int[]{0, 1, 2, 3, 0});
+        System.out.println(Arrays.toString(targetArray));
+    }
+
+    public static int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > index[i]; j--) {
+                target[j] = target[j - 1];
+            }
+            target[index[i]] = nums[i];
+        }
+
+        return target;
+    }
+}
+/*
+     [0,0,0,0,0]
+     [0,1,0,0,0]
+     [0,1,2,0,0]
+     [0,1,3,2,0]
+     [0,4,1,3,2]
+ */
--- src/datatype/PowerOfFour.java
+++ src/datatype/PowerOfFour.java
@@ -9,4 +9,12 @@ public class PowerOfFour {
     public static boolean isPowerOfFour(int n) {
         return (Math.log(n) / Math.log(4)) % 1 == 0;
     }
+
+    public static boolean isPowerOfFour1(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        double logValue = Math.log(n) / Math.log(4);
+        return Math.floor(logValue) == logValue;
+    }
 }
