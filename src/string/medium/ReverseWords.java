3a08451214a1985123545cbd0306c5caee290d4c
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-03-18 00:16:56 +0400

    solved Reverse Words in a String problem

---
+++ src/string/medium/ReverseWords.java
@@ -0,0 +1,17 @@
+package string.medium;
+
+// 151
+public class ReverseWords {
+    public String reverseWords(String s) {
+        StringBuilder reverseString = new StringBuilder();
+        s = s.trim();
+
+        String[] array = s.split("\\s");
+        for (int i = 0; i < array.length; i++) {
+            if (!array[array.length - 1 - i].isEmpty())
+                reverseString.append(array[array.length - 1 - i]).append(" ");
+        }
+
+        return reverseString.toString().trim();
+    }
+}
