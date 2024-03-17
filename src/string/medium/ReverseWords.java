8294cc24b49a75c52725b3502bed427a41e75b6f
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-03-18 00:22:51 +0400

    added second solution Reverse Words in a String problem

--- src/string/medium/ReverseWords.java
+++ src/string/medium/ReverseWords.java
@@ -14,4 +14,16 @@ public class ReverseWords {
 
         return reverseString.toString().trim();
     }
+
+    public String reverseWords1(String s) {
+        StringBuilder reverseString = new StringBuilder();
+        s = s.trim();
+
+        String[] array = s.split("\\s+");
+        for (int i = 0; i < array.length; i++) {
+            reverseString.append(array[array.length - 1 - i]).append(" ");
+        }
+
+        return reverseString.toString().trim();
+    }
 }
