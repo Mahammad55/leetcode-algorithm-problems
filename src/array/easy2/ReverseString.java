a33054d07b771c17ff0879afacb0c6473dd80d77
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-04-02 11:04:37 +0400

    solved Reverse String problem

---
+++ src/array/easy2/ReverseString.java
@@ -0,0 +1,20 @@
+package array.easy2;
+
+import java.util.Arrays;
+
+// 344
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public static void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - i - 1];
+            s[s.length - i - 1] = temp;
+        }
+    }
+}
