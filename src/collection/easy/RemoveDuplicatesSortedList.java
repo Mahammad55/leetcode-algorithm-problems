675d8fc65df10b1a955832663d1111c2130510dd
mahammad.ilyazov <ilyazovmehemmed@gmail.com>
2025-06-03 00:49:12 +0400

    solved Remove Duplicates from Sorted List problem

---
+++ src/collection/easy/RemoveDuplicatesSortedList.java
@@ -0,0 +1,42 @@
+package collection.easy;
+
+// 83
+public class RemoveDuplicatesSortedList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode list = head;
+
+        while (list != null) {
+            if (list.next == null) {
+                break;
+            }
+            if (list.val == list.next.val) {
+                list.next = list.next.next;
+            } else {
+                list = list.next;
+            }
+        }
+
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
