b175d1b65f05d31e3c4ad932a2b735d18b66f80c
mahammad.ilyazov <ilyazovmehemmed@gmail.com>
2026-04-05 23:31:43 +0400

    solved Biggest Single Number problem

---
+++ src/database/easy/BiggestSingleNumber.java
@@ -0,0 +1,14 @@
+package database.easy;
+
+// 619
+public class BiggestSingleNumber {
+
+//    select max(num) as num
+//    from (
+//            select num
+//    from MyNumbers
+//            group by num
+//            having count(num)=1
+//            ) as unique_num
+
+}
