1af829a13201ac834082fdc40f1d59d8b5715def
mahammad.ilyazov <ilyazovmehemmed@gmail.com>
2026-04-05 23:44:52 +0400

    solved Day of the Week problem

---
+++ src/date/easy/DayOfTheWeek.java
@@ -0,0 +1,26 @@
+package date.easy;
+
+import java.time.LocalDate;
+
+// 1185
+public class DayOfTheWeek {
+    public static void main(String[] args) {
+        System.out.println(new DayOfTheWeek().dayOfTheWeek(5, 4, 2026));
+    }
+
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+        return date.getDayOfWeek().name().charAt(0) + date.getDayOfWeek().name().substring(1).toLowerCase();
+    }
+
+    public String dayOfTheWeek1(int day, int month, int year) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        LocalDate date = LocalDate.of(year, month, day);
+
+        return days[date.getDayOfWeek().getValue() - 1];
+    }
+
+    // Zeller's Congruence 
+
+}
